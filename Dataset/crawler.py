6e07d0713f63abebb7da75d417374eb9d0fb8721
Seongyeon <115054681+deepshadow25@users.noreply.github.com>
2024-06-15 05:21:36 +0900

    Update crawler.py

--- Dataset/crawler.py
+++ Dataset/crawler.py
@@ -1,16 +1,18 @@
 ## 사용방법
 """
-python crawler.py --date 2024-04-20
+python crawler.py
 """
 
 # Import Library
 from requests import get
 from requests.compat import urljoin
 from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from fake_useragent import UserAgent
+
 from urllib3.exceptions import *
 from requests.exceptions import *
+
 from tqdm import tqdm
 import pandas as pd
 import argparse
@@ -19,8 +21,9 @@ import json
 import time
 
 import mysql.connector
-from mysql.connector import Error
+from mysql.connector import Error, connect
 from datetime import datetime
+
 
 # 전처리 함수 정의
 def preprocessing(d):
@@ -29,31 +32,10 @@ def preprocessing(d):
     d = re.sub(r'‘’ⓒ\'\"“”…=□*◆:/_]', ' ', d)
     d = re.sub(r'\s+', ' ', d)
     d = re.sub(r'^\s|\s$', '', d)
-    # d = re.sub(r'[\<br\>]','',d)
     d = re.sub(r'[<*>_="/]', '', d)
     return d
 
 # Base URL 설정
-"""
-페이지수를 볼 수 있는 네이버 뉴스 주소 
-(일반 뉴스는 .../main/main.naver?... 주소로 이루어짐)
-'https://news.naver.com/main/list.naver?
-mode=LSD&
-mid=shm&
-sid1=100&   -- 카테고리
-date={date}& -- 상세카테고리
-page={page}' -- 페이지
-
-sid1 (카테고리) : 100(정치), 101(경제), 102(사회), 103(생활문화), 104(세계), 105(IT과학), 106(연예), 107(스포츠), 110(오피니언)
-
-sid2 (상세카테고리) : 
-"""
-
-
-
-
-# Base URL 설정
-
 base_url = 'https://news.naver.com/main/list.naver?mode=LSD&mid=shm&date={date}&page={page}'
 ua = UserAgent()
 headers = {'User-Agent':ua.random}
@@ -89,7 +71,6 @@ def fetch_articles(url):
         article_dict = {
             'title': title_text, # 뉴스제목
             'link': article_url, # 뉴스링크
-            'media': source_tag.get_text(strip=True) if source_tag else '' # 언론사
         }
         
         # 여러 기사에 대해 메타데이터 작성
@@ -112,58 +93,54 @@ def generate_dates(date):
         yield current_date
         current_date += timedelta(days=1)
 
-# 댓글 통계 가져오는 함수
-
         
-# 기사 본문, 기자 정보 수집 함수
+# 기사 본문 수집 함수
 def fetch_article_data(article_url):
     resp = get(article_url, headers=headers)
     article_dom = BeautifulSoup(resp.text, 'html.parser')
-
 
     # 기사 본문 추출
     content_tag = article_dom.select_one('article')
     
     # 이미지 태그, 요약 태그 걸러내기 : 클러스터링 성능 강화
     # 동영상은 별도 태그여서 본 과정 안 거침
-    img_tag = content_tag.find('img')
-    if img_tag:
-        img_tag.extract()
+    if content_tag is None:
+        pass
+    else:
+        img_tag = content_tag.find('img')
+        if img_tag:
+            img_tag.extract()
+            
+        em_tag = content_tag.find('em')
+        if em_tag:
+            em_tag.extract()
         
-    em_tag = content_tag.find('em')
-    if em_tag:
-        em_tag.extract()
-    
-    sum_tag = content_tag.find('strong')
-    if sum_tag:
-        sum_tag.extract()
-    
+        sum_tag = content_tag.find('strong')
+        if sum_tag:
+            sum_tag.extract()
+        
     # 기사 본문 전처리
     content = preprocessing(content_tag.get_text(strip=True)) if content_tag else ''
     
-    # # 문장 수 제한 (10문장 이상)
+    # 문장 수 제한 (10문장 이상)
     if len(content.split('.')) < 10:
         content = ''
- 
-    # 기자 정보 추출
-    reporter_tag = article_dom.select_one('div.byline span')
-    reporter = reporter_tag.get_text(strip=True) if reporter_tag else ''
 
     article_data = {
         'link': article_url, # 기사링크
         'article': content,  # 기사본문
-        'reporter': reporter # 기자
-    }
+        }
 
     return article_data
 
 
-## 기사 본문 추가 전처리 함수
+# 기사 본문 추가 전처리 함수
 def refine_article(df):
-    # 기사 끝부분 불필요한 부분
-    # ".....입니다. 제보 ~~ 더보기 등." 에서 제보 부분부터 걸러내는 방식.
+    """
+    기사 끝부분 불필요한 부분  거르는 함수
+    ".....입니다(기사 내용). 제보 ~~ 더보기 등." 에서 제보 부분부터 걸러내는 방식.
+    """
     m5_list = ['더팩트']
-    # m3_list = ['kbc광주방송']
     m2_list = ['YTN', 'MBC', 'KBS', 'MBN', '채널A', 'kbc광주방송']
     m1_list = ['SBS', 'TV조선', 'JTBC']
     
@@ -188,8 +165,6 @@ def refine_article(df):
         # media에 따른 article 수정
         if media in m5_list:
             new_articles.append(','.join(article.split('.')[:-5]))
-        # elif media == m3_list:
-        #     new_articles.append(','.join(article.split('.')[:-3]))
         elif media in m2_list:
             new_articles.append(','.join(article.split('.')[:-2]))
         elif media in m1_list:
@@ -202,51 +177,30 @@ def refine_article(df):
     
     return df
 
-def collect_news():
-    # 여기서 뉴스 데이터를 수집하는 로직을 작성합니다.
-    # 예를 들어, 뉴스 데이터는 다음과 같은 형식으로 수집될 수 있습니다.
-    news_data = [
-        {
-            'title': 'Sample News Title 1',
-            'content': 'This is a sample news content 1.',
-            'media': 'Sample Media 1',
-            'reporter': 'Reporter 1',
-            'link': 'https://sample.news/link1',
-            'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        },
-        {
-            'title': 'Sample News Title 2',
-            'content': 'This is a sample news content 2.',
-            'media': 'Sample Media 2',
-            'reporter': 'Reporter 2',
-            'link': 'https://sample.news/link2',
-            'date': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        }
-    ]
-
-    for news in news_data:
-        save_to_db(news)
-
-def save_to_db(news_data):
+# MySQL DB(서버) 연결 함수
+def create_connection(host_name, user_name, user_password, db_name):
     try:
-        connection = mysql.connector.connect(
-            host='localhost',
-            database='news_db',
-            user='news_user',
-            password='your_password'
+        connection = connect(
+            host=host_name,
+            user=user_name,
+            password=user_password,
+            database=db_name
         )
-        cursor = connection.cursor()
-        insert_query = """
-        INSERT INTO news (title, content, media, reporter, link, date) 
-        VALUES (%s, %s, %s, %s, %s, %s)
-        """
-        cursor.execute(insert_query, (news_data['title'], news_data['content'], news_data['media'], news_data['reporter'], news_data['link'], news_data['date']))
-        connection.commit()
-        cursor.close()
-        connection.close()
-        print(f"News '{news_data['title']}' added successfully!")
+        print("Connection to MySQL DB successful")
     except Error as e:
-        print(f"Error: {e}")
+        print(f"The error '{e}' occurred")
+        return None
+    return connection
+
+# MySQL 쿼리문 실행 함수
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query executed successfully")
+    except mysql.connector.Error as e:
+        print(f"The error '{e}' occurred")
 
 
 ## 기사 메타데이터 합치기
@@ -269,22 +223,17 @@ def main(date_str):
                 all_articles.extend(article_data_list)
                 url = get_next_page(dom, page)
                 page += 1
-                time.sleep(1)  # 서버에 부담을 주지 않도록 잠시 대기
+                time.sleep(0.7)  # 서버에 부담을 주지 않도록 잠시 대기
 
         # # 메타데이터 데이터프레임 생성 + 메타데이터 백업
         df1 = pd.DataFrame(all_articles)
         df1.drop_duplicates(ignore_index=True)
-        metadata_filename = f'metadata_{str(date)[:10]}.csv'
-        df1.to_csv(f'./Naver_News/{metadata_filename}', index=False, encoding='utf-8-sig')
-
-        print(f"메타데이터를 파일로 저장했습니다: {metadata_filename}")
-
-        # 기사 본문, 기자 정보 리스트에 저장하기
-        # df1 = pd.read_csv(f'./Naver_News/{metadata_filename}', encoding='utf-8-sig')
+                
         article_data_list = []
         for url in tqdm(list(df1['link'])):
             article_data = fetch_article_data(url)
             article_data_list.append(article_data)
+            time.sleep(0.7)
 
         # 본문, 기자 정보 데이터프레임 만들기
         df2 = pd.DataFrame(article_data_list)
@@ -293,42 +242,66 @@ def main(date_str):
         df = pd.merge(df1, df2)
 
         # 데이터프레임 열 순서 바꾸기
-        df = df[['media', 'reporter', 'title', 'link', 'article']]
+        df = df[['title', 'link', 'article']]
 
         # 중복, 빈 본문 기사 제거
         df.drop_duplicates(subset=None, keep='first', inplace=True, ignore_index=True)
         
-        # 기사 본문 추가전처리
-        
+        # 기사 본문 추가전처리, 빈 본문 행 제거
         df = refine_article(df)
+        df.dropna(subset=['article'], inplace=True)
         
         # 열 이름 변경
-        df.columns = ['media', 'reporter', 'title', 'link', 'article']
+        df.columns = ['title', 'link', 'article']
         
-        df.to_csv(f'./Naver_News/{csv_filename}', index=False, encoding='utf-8-sig')
+        # SQL 서버 연결. "db" DB에 연결
+        # 언론사, 기자, 기사제목, 기사링크, 기사본문을 기본 데이터로 지정 + "news" 테이블에 정보 저장
+        connection = create_connection(host_name, user_name, user_password, db_name)
+        if connection:
+            metadata_table_sql = """
+            CREATE TABLE IF NOT EXISTS news (
+                title TEXT,
+                link VARCHAR(255) UNIQUE,
+                article TEXT
+            )
+            """
+            execute_query(connection, metadata_table_sql)
+            
+            insert_sql = """
+            INSERT IGNORE INTO news (title, link, article)
+            VALUES (%s, %s, %s, %s, %s)
+            """
+            data = [tuple(x) for x in df.values]
+            cursor = connection.cursor()
+            cursor.executemany(insert_sql, data)
+            connection.commit()
+            print("Data inserted successfully")
+
+            # 중복데이터 별도 처리 단계 (중복 제거되지 않을 경우 추가로 처리)
+            cleanup_queries = [
+                "CREATE TEMPORARY TABLE news_temp AS SELECT * FROM news WHERE article IS NOT NULL AND TRIM(article) != '';",
+                "TRUNCATE TABLE news;",
+                "INSERT INTO news SELECT * FROM news_temp;",
+                "DROP TABLE news_temp;"
+            ]
+            for query in cleanup_queries:
+                execute_query(connection, query)
+                
+            
+            connection.close()
         
-        return df
-        
-    except (ProtocolError, ConnectionError, UnboundLocalError, ChunkedEncodingError) as e:
+    except (ProtocolError, ConnectionError, UnboundLocalError, ChunkedEncodingError, mysql.connector.Error) as e:
         print (f'{e} occured..!!')
-        return df
-
-    finally:
-        # CSV 파일로 저장
         
-        df = pd.read_csv(f'./Naver_News/{csv_filename}', encoding='utf-8-sig')
-        df.dropna(subset=['article'], inplace=True, axis=0)
-        df.to_csv(f'./Naver_News/{csv_filename}', index=False, encoding='utf-8-sig')
 
-        print(f"CSV 파일로 저장되었습니다: {csv_filename}")
-    
-    
-# parser 작동 구문
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='뉴스 기사 크롤러')
-    parser.add_argument('--date', type=str, required=True, help='수집할 기사의 발행일자 (예: 2024-04-20)')
-    args = parser.parse_args()
-
-    main(args.date)
+    # MySQL 서버 연결에 필요한 ip주소, 사용자 이름, 비밀번호, db이름 설정
+    host_name = "DB.Server.Ip.Address" # ip 주소
+    user_name = "Username" # 사용자 이름
+    user_password = "Password" # 비밀번호
+    db_name = "DBName"     # db 이름
+    
+    main((date.today()).strftime("%Y-%m-%d"))
 
